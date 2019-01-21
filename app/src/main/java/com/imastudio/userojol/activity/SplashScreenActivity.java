250772305f66b5cc08f1121538ce727f5e18d963
udakita <udakita11@gmail.com>
2019-01-21 10:10:02 +0700

    Initial commit

---
+++ app/src/main/java/com/imastudio/userojol/activity/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.imastudio.userojol.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
